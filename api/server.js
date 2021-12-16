f70c05efdff69de9bd984f5d8f7c29bcfb92b19e
ST1414 <sterry1414@gmail.com>
2021-12-16 14:09:27 -0800

    index.js, server.js, and cars-router.js are up and running

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const server = express();
+const carsRouter = require('./cars-router');
+
+server.use(express.json());
+server.use('/api/cars', carsRouter)
+server.get('/', (req, res) => {
+    res.send('<h1>Hooray!  You found us!</h1>');
+})
+
+module.exports = server;
